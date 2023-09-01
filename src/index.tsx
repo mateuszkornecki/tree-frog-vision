789a26107f568802c9db42f4e090a353ff7fbc82
Mateusz Kornecki <mateusz.kornecki@hey.com>
2023-09-01 08:55:54 +0200

    refactor: deleted unused components

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,6 @@
 // import './index.css';
 import React from 'react';
-import ReactDOM from 'react-dom/client';
 import App from './components/App';
-import reportWebVitals from './reportWebVitals';
 
 export default () => {
     return (
